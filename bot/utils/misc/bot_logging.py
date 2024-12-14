c61965ddd4c99667aab3eac79d0845e44ba84611
tw1xteam <tw1xteamsoft@gmail.com>
2024-12-14 21:01:40 +0300

    Fix the logging

--- bot/utils/misc/bot_logging.py
+++ bot/utils/misc/bot_logging.py
@@ -8,7 +8,7 @@ from bot.data.config import PATH_LOGS
 # Формат логгирования
 log_formatter_file = bot_logger.Formatter("%(levelname)s | %(asctime)s | %(filename)s:%(lineno)d | %(message)s")
 log_formatter_console = colorlog.ColoredFormatter(
-    "%(purple)s%(levelname)s %(blue)s|%(purple)s %(asctime)s %(blue)s|%(purple)s %(filename)s:%(lineno)d %(blue)s|%(purple)s %(message)s%(red)s",
+    "%(red)s%(levelname)s %(blue)s|%(white)s %(asctime)s %(blue)s|%(green)s %(filename)s:%(lineno)d %(blue)s|%(purple)s %(message)s%(red)s",
     datefmt="%d-%m-%Y %H:%M:%S",
 )
 
