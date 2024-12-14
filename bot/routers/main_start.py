12536e56489b7aaa560a154f9c33bb4714d4d4e0
tw1xteam <tw1xteamsoft@gmail.com>
2024-12-14 20:29:43 +0300

    Added the main_start.py router and handling /start message.

---
+++ bot/routers/main_start.py
@@ -0,0 +1,23 @@
+# - *- coding: utf- 8 - *-
+from aiogram import Router, Bot, F
+from aiogram.filters import StateFilter
+from aiogram.types import Message, CallbackQuery
+
+from bot.utils.const_functions import ded
+from bot.utils.misc.bot_models import FSM, ARS
+
+router = Router(name=__name__)
+
+
+@router.message(F.text == '/start')
+async def main_start(message: Message, bot: Bot, state: FSM, arSession: ARS):
+    await state.clear()
+
+    await message.answer(
+        ded("""
+            🔸 Бот готов к использованию.
+            🔸 Если не появились вспомогательные кнопки
+            🔸 Введите /start
+        """),
+        reply_markup=menu_frep(message.from_user.id),
+    )
